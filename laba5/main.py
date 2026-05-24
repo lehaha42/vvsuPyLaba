769aadcc607b65bf2dce3b70b592bf35ae3a03c0
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2026-05-24 14:40:46 +1000

    cant make it work without human verification

--- laba5/main.py
+++ laba5/main.py
@@ -3,16 +3,21 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://openlibrary.org"
+    url = 'https://openlibrary.org' + '/search?q=ddc%3A8%2A+first_publish_year%3A%5B%2A+TO+1950%5D+publish_year%3A%5B2000+TO+%2A%5D+NOT+public_scan_b%3Afalse+language%3Arus+-subject%3A"content_warning%3Acover"+-subject%3A"content_warning%3Acover"&sort=trending'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.1 Safari/537.36'
     }
-    response = requests.get(url, headers=headers)
+    cookies = {'donation-identifier': 'MC4xMDI3NDQ3MTc5NDc2MzA5Ng=='}
+    response = requests.get(url, headers=headers, cookies=cookies)
 
-    if response.status_code == 200:
-        print(response.text)  # HTML-код страницы
-    else:
+    if response.status_code != 200:
         print(f"Ошибка: {response.status_code}")
+        return
+    if "Verification failed. Please try again." in response.text:
+        print("Verification failed")
+        return
+    soup = BeautifulSoup(response.text, "html.parser")
+    print(soup.prettify())
 
 
 if __name__ == "__main__":
